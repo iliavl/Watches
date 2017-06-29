224db8fd0d30e5beba0628803aba3032a29e0cba
Ïlia Lisin <liavl@icloud.com>
2017-06-29 17:19:12 +0300

    first

---
+++ HomeWork4/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HomeWork4
+//
+//  Created by LIV on 23.06.17.
+//  Copyright © 2017 LIV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    
+    
+    
+
+}
+
---
+++ HomeWork4/Watches.swift
@@ -0,0 +1,19 @@
+//
+//  Watches.swift
+//  HomeWork4
+//
+//  Created by LIV on 23.06.17.
+//  Copyright © 2017 LIV. All rights reserved.
+//
+
+import UIKit
+
+class Watches: NSObject {
+
+//    var gear: Bool = false
+//    var color: String
+//    let doorsCount: Int = 4
+//    var speed: Int = 0
+//    let brand: String
+
+}
